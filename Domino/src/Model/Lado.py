f48bd54897b9275ce9c9c23f10fb4e19cf5c8ba5
Ntheus <matheusoliveira.7313@gmail.com>
2023-11-29 00:52:08 -0300

    retorna o lado conectado, desenha sua peca, set conexao

--- Domino/src/Model/Lado.py
+++ Domino/src/Model/Lado.py
@@ -3,15 +3,20 @@ from __future__ import annotations
 class Lado:
     valor:int = None
     conexao = None
+    peca = None
     
     def __init__(self, valor:int) -> None:
         self.valor = valor
+        self.conexao = None
+        self.peca = None
         pass
     
     def conecta(self, lado:Lado) -> bool:
         if(lado.conexao == None and self.getValor() == lado.getValor()):
-            self.conexao == lado
-            lado.conecta()
+            self.setConexao(lado)
+            lado.setConexao(self)
+            print(self.conexao)
+            print(lado.conexao)
             return True
         return False
     
@@ -21,5 +26,20 @@ class Lado:
     def getValor(self):
        return self.valor
    
-    def getConexao(self):
+    def getConexao(self) -> Lado:
        return self.conexao
+    
+    def getPecaAqualPerteco(self):
+        return self.peca
+    
+    def getDesenheMinhaPeca(self):
+        self.peca.desenhar()
+   
+    def conexaoFazParteDaPeca(self):
+       self.conexao.desenhar()
+       
+    def setConexao(self,lado:Lado):
+        self.conexao = lado
+        
+    def setPeca(self,peca):
+        self.peca = peca
