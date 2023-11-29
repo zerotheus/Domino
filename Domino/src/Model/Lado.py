f6a9f7fdd7fdfc3a8d03c6b2ca27894646aa09cc
Ntheus <matheusoliveira.7313@gmail.com>
2023-11-28 21:32:50 -0300

    lado possui conexao agora

--- Domino/src/Model/Lado.py
+++ Domino/src/Model/Lado.py
@@ -2,16 +2,24 @@ from __future__ import annotations
 
 class Lado:
     valor:int = None
+    conexao = None
     
     def __init__(self, valor:int) -> None:
         self.valor = valor
         pass
     
     def conecta(self, lado:Lado) -> bool:
-        return self.getValor() == lado.getValor()
+        if(lado.conexao == None and self.getValor() == lado.getValor()):
+            self.conexao == lado
+            lado.conecta()
+            return True
+        return False
     
     def conectaComEstaPeca(self, peca) -> bool:
         self.conecta(peca.getladoSuperior()) or self.conecta(peca.getladoInferior())
         
     def getValor(self):
        return self.valor
+   
+    def getConexao(self):
+       return self.conexao
