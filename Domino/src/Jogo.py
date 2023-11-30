07b72af66898f0974c62a24f56b857fd57ec8938
Alana <alana__santos1503@hotmail.com>
2023-11-30 00:45:28 -0300

    coloquei a variavel lado para testar

--- Domino/src/Jogo.py
+++ Domino/src/Jogo.py
@@ -1,18 +1,23 @@
 import pygame
 import os
 import random
+from Model.Lado import Lado
+from Model.Peca import Peca
 from Model.Tela import TELA_ALTURA, TELA_LARGURA, tela
 class Jogo:
-
-    
     IMAGEM_DE_FUNDO = pygame.image.load(os.path.join('Domino\pecasDomino','tela.png'))
 
     pygame.font.init()
     FONTE_JOGO = pygame.font.SysFont('arial',50)
 
     def desenharTela(self):
+        lado1 = Lado(0)
+        lado2 = Lado(0)
+        
+        peca = Peca(lado1,lado2)
         cenarioRetangulo = pygame.Rect(0, 0, TELA_LARGURA, TELA_ALTURA)
         tela.blit(self.IMAGEM_DE_FUNDO,cenarioRetangulo)
+        peca.desenhar(tela)
         pygame.display.update()
 
     
