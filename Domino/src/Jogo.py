1f1d288bfb23310c123030bd1c770cc11dd68e3b
Ntheus <matheusoliveira.7313@gmail.com>
2023-11-30 11:58:53 -0300

    embaralha, get jogador, informa qtd de pecas de sorteio

--- Domino/src/Jogo.py
+++ Domino/src/Jogo.py
@@ -12,7 +12,11 @@ class Jogo:
 
     caixaDeDomino:CaixaDeDomino = None
     pecasJogadas:list[Peca] = []
+    pecaLivreLadoEsquerdo:Peca = None
+    pecaLivreLadoDireito:Peca = None
+    pecasParaSortear:list[Peca] = []
     participantes = []
+    jogador = None
 
     IMAGEM_DE_FUNDO = pygame.image.load(os.path.join('Domino\pecasDomino','tela.png'))
 
@@ -21,6 +25,9 @@ class Jogo:
 
     def __init__(self) -> None:
         self.caixaDeDomino = CaixaDeDomino()
+        print(self.caixaDeDomino)
+        self.jogador = Jogador("Sofia dahPuta")
+        self.pecasParaSortear = self.caixaDeDomino.getPecas()
         pass
 
     def desenharTela(self):
@@ -35,4 +42,22 @@ class Jogo:
 
     def addParticipante(self, participante):
         self.participantes.append(participante)
+        pass
+    
+    def embaralhar(self):
+        pecas = self.pecasParaSortear #TODO alterar como funciona
+        for i in range (0,7):
+            index = random.randint(0,len(pecas) - 1)
+            peca = pecas[index]
+            self.jogador.setPeca(peca)
+            pecas.remove(peca)
+        pass
+    
+    def getJogador(self):
+        return self.jogador
+    
+    def quantidadeDePecas(self):
+        print("pecas em jogo",len(self.pecasParaSortear))
+    
+    def defineOrdemDeJogada(self):
         pass
