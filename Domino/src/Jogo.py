3b29448eb45158d3b520628c6063f98810b39aa3
Ntheus <matheusoliveira.7313@gmail.com>
2023-11-30 00:52:26 -0300

    inicializando caixa, jogadores e pecas jogadas

--- Domino/src/Jogo.py
+++ Domino/src/Jogo.py
@@ -2,17 +2,30 @@ import pygame
 import os
 import random
 from Model.Tela import TELA_ALTURA, TELA_LARGURA, tela
+from Model.Peca import Peca
+from Model.Jogador import Jogador
+from Model.CaixaDeDomino import CaixaDeDomino
+
 class Jogo:
 
-    
+    caixaDeDomino:CaixaDeDomino = None
+    pecasJogadas:list[Peca] = []
+    participantes = []
+
     IMAGEM_DE_FUNDO = pygame.image.load(os.path.join('Domino\pecasDomino','tela.png'))
 
     pygame.font.init()
     FONTE_JOGO = pygame.font.SysFont('arial',50)
+
+    def __init__(self) -> None:
+        self.caixaDeDomino = CaixaDeDomino()
+        pass
 
     def desenharTela(self):
         cenarioRetangulo = pygame.Rect(0, 0, TELA_LARGURA, TELA_ALTURA)
         tela.blit(self.IMAGEM_DE_FUNDO,cenarioRetangulo)
         pygame.display.update()
 
-    
+    def addParticipante(self, participante):
+        self.participantes.append(participante)
+        pass
