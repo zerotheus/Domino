136e059ff5dcf136be69d56ed30c3bdd0db3d4a6
Ntheus <matheusoliveira.7313@gmail.com>
2023-11-30 13:02:26 -0300

    seleciona jogador com a maior peca para comecar

--- Domino/src/Jogo.py
+++ Domino/src/Jogo.py
@@ -12,8 +12,9 @@ class Jogo:
 
     caixaDeDomino:CaixaDeDomino = None
     pecasJogadas:list[Peca] = []
-    pecaLivreLadoEsquerdo:Peca = None
-    pecaLivreLadoDireito:Peca = None
+    buxaDeSena:Peca = None 
+    pecaLivreLadoEsquerdo:Peca = None#pra cima esquerda
+    pecaLivreLadoDireito:Peca = None#pra cima direita
     pecasParaSortear:list[Peca] = []
     participantes = []
     jogador = None
@@ -27,7 +28,12 @@ class Jogo:
         self.caixaDeDomino = CaixaDeDomino()
         print(self.caixaDeDomino)
         self.jogador = Jogador("Sofia dahPuta")
+        self.addParticipante(self.jogador)
+        self.addParticipante(Jogador("Yasmin Yaz Bollaz"))
+        self.addParticipante(Jogador("Shotaberta"))
+        self.addParticipante(Jogador("PowerGuido"))     
         self.pecasParaSortear = self.caixaDeDomino.getPecas()
+        self.buxaDeSena = self.pecasParaSortear[27]
         pass
 
     def desenharTela(self):
@@ -45,11 +51,16 @@ class Jogo:
         pass
     
     def embaralhar(self):
+        for participante in self.participantes:
+            self.entregePecas(participante)
+        pass
+    
+    def entregePecas(self,participante):
         pecas = self.pecasParaSortear #TODO alterar como funciona
         for i in range (0,7):
             index = random.randint(0,len(pecas) - 1)
             peca = pecas[index]
-            self.jogador.setPeca(peca)
+            participante.setPeca(peca)
             pecas.remove(peca)
         pass
     
@@ -60,4 +71,14 @@ class Jogo:
         print("pecas em jogo",len(self.pecasParaSortear))
     
     def defineOrdemDeJogada(self):
+        index = self.pegajogadorComMaiorBuxa()
+        print("Jogador", index , "tem a buxa de sena")
+
+    
+    def pegajogadorComMaiorBuxa(self):
+        for i in range (0,4):
+            if(self.participantes[i].tenhoBuxa() == self.buxaDeSena):
+                return i
+    
+    def aindaEhPossivelDeJogar(self):
         pass
